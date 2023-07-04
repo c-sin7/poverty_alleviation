9a64dd57af8af6fbab4d8a722b5e097f0ab1f334
sin <1127301986@qq.com>
2023-07-04 12:08:40 +0800

    init

---
+++ src/main/java/com/example/poverty_alleviation/PovertyAlleviationApplication.java
@@ -0,0 +1,13 @@
+package com.example.poverty_alleviation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PovertyAlleviationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PovertyAlleviationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poverty_alleviation/PovertyAlleviationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poverty_alleviation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PovertyAlleviationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
